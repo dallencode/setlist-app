d32ad87347715090db8e764c8666c8a33f4a0dc7
dallencode <dallencodes@gmail.com>
2022-04-18 11:46:20 +1000

    added song.rb

---
+++ src/song.rb
@@ -0,0 +1,23 @@
+class Song
+    def initialize
+        @arr = []
+    end
+
+    def add(name, artist)
+        @arr << {name: name, artist: artist}
+    end
+
+    def remove(name)
+        @arr.delete_if {|h| h[:name] == name}
+    end
+
+    def push_to_setlist
+       @arr
+    end
+end
+
+list = Song.new
+list.add("hello", "adele")
+# list.remove("hello")
+list.add("123", '12')
+p list.push_to_setlist
