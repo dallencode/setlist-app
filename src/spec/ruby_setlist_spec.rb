efac89efbae41008b1df0a8d0baac1e0221208a4
dallencode <dallencodes@gmail.com>
2022-04-18 14:46:01 +1000

    added setlist.rb testing

--- src/spec/ruby_setlist_spec.rb
+++ src/spec/ruby_setlist_spec.rb
@@ -1,26 +1,9 @@
-require_relative '../song'
-# require_relative '../user'
 require_relative '../setlist'
-
-describe Song do
-    it 'should add new song to array' do
-        name = "Hello"
-        artist = "Adele"
-        song = Song.new
-        expect(song.add(name, artist)).to eq([{name: "Hello", artist: "Adele"}])
-    end
-
-    it 'should remove song from array' do
-        name = "Hello"
-        artist = "Adele"
-        arr = [{name: "Hello", artist: "Adele"}]
-        song = Song.new
-        expect(song.remove(name)).to eq([])
-    end
-end
 
 describe Setlist do
     it 'should display current set list' do
+        arr = [{name: "Hello", artist: "Adele"}]
         setlist = Setlist.new
+        expect(setlist.view(arr)).to eq("1. Hello by Adele")
     end
 end
