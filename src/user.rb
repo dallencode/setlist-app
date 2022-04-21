4a601086a3dce552486f748b5ddc8a32ff4fb803
dallencode <dallencodes@gmail.com>
2022-04-21 10:43:04 +1000

    added dynamic file exist checking

--- src/user.rb
+++ src/user.rb
@@ -4,20 +4,20 @@ require_relative 'setlist'
 class User
     attr_reader :name
 
-    def user_name
+    def check_user_exists
         puts "What is your name?"
-        @name = gets.chomp.capitalize
+        @name = gets.chomp.titleize
+        puts File.file?("#{@name.downcase}.json") ? "Welcome back, #{@name}!" : "Welcome #{@name}!"
     end
 
-    def welcome
-        puts "Welcome #{@name}!"
-    end
 end
 
-# p 'working' if File.file?('starter_list.json')
-file = File.read('starter_list.json')
-data_hash = JSON.parse(file, :symbolize_names => true)
+user = User.new
+user.check_user_exists
 
-data_hash.shuffle.each_with_index do |item, index| 
-    puts "#{index + 1}. #{item[:name]} by #{item[:artist]}"
-end
+# file = File.read('starter_list.json')
+# data_hash = JSON.parse(file, :symbolize_names => true)
+
+# data_hash.shuffle.each_with_index do |item, index| 
+#     puts "#{index + 1}. #{item[:name]} by #{item[:artist]}"
+# end
